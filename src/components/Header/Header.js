cad9e7e07f494e024f3209e2a2fde24b83489115
subustark <subustark25@gmail.com>
2023-04-19 17:15:48 +0530

    resume link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,7 +25,7 @@ const Header = ({ toggle }) => {
         <NavBtn>
           <a
             className="btn PrimaryBtn"
-            href="https://github.com/subustark"
+            href="https://drive.google.com/file/d/1wFM67qhtzLxAQcuaPJzG_xgvtszVsUf9/view?usp=share_link"
             target="_blank"
             rel="noopener noreferrer"
           >
